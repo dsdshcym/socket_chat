1891e195f31c82041d9b8720e210436188ed6e15
Yiming Chen <dsdshcym@gmail.com>
2015-11-24 20:20:40 +0800

    Add test for server.parse

---
+++ test/test_server.rb
@@ -0,0 +1,27 @@
+require "test/unit"
+require "shoulda"
+require_relative "../lib/socket_chat/server.rb"
+
+class TestServer < Test::Unit::TestCase
+  def setup
+    @server = Server.new("localhost", 8000)
+  end
+
+  context "parsing message" do
+    setup do
+      @correct_args           = ["LOGIN", "test"]
+      @message_without_spaces = "LOGIN test"
+      @message_with_spaces    = "    LOGIN test   "
+    end
+
+    should "correctly parse a message without leading and trailing spaces" do
+      args = @server.send(:parse, @message_without_spaces)
+      assert_equal @correct_args, args
+    end
+
+    should "correctly parse a message with leading and trailing spaces" do
+      args = @server.send(:parse, @message_with_spaces)
+      assert_equal @correct_args, args
+    end
+  end
+end
