69a3b205acf14a5ebf6653c9d80da40776b8b917
Yiming Chen <dsdshcym@gmail.com>
2015-11-24 19:49:14 +0800

    Add server to run and listen from clients

---
+++ lib/socket_chat/server.rb
@@ -0,0 +1,28 @@
+require "socket"
+
+class Server
+  def initialize(host, port)
+    @server = TCPServer.open(host, port)
+    @users = Hash.new
+    @channels = Hash.new
+    @clients = Hash.new
+  end
+
+  def run
+    loop {
+      Thread.start(@server.accept) do |client|
+        listen client
+      end
+    }.join
+  end
+
+  private
+
+  def listen(client)
+    loop {
+      message = client.gets.chomp
+      args = parse(message)
+      self.send(args[0].downcase.to_sym, client, args[1..-1])
+    }
+  end
+end
