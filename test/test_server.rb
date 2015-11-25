f3d435823968f6ba0da76bc69024ad75d210eac5
Yiming Chen <dsdshcym@gmail.com>
2015-11-25 19:16:22 +0800

    Add test for user login

--- test/test_server.rb
+++ test/test_server.rb
@@ -1,6 +1,8 @@
 require "test/unit"
 require "shoulda"
+require "json"
 require_relative "../lib/socket_chat/server.rb"
+require_relative "../lib/socket_chat/user.rb"
 
 class TestServer < Test::Unit::TestCase
   context "Server" do
@@ -26,6 +28,51 @@ class TestServer < Test::Unit::TestCase
       end
     end
 
+    context "user login" do
+      setup do
+        @logged_username = "Alice"
+        logged_user = User.new(@logged_username)
+        logged_client = Client.new(:logged_client)
+        @logged_client = logged_client
+        @server.instance_eval do
+          @clients[logged_client] = logged_user
+        end
+        @new_client = Client.new(:new_client)
+        @new_username = "Bob"
+      end
+
+      should "successful using another client" do
+        @server.send(:login, @new_client, @new_username)
+        response_json = @new_client.response
+        result = JSON.parse(response_json)
+        assert_true result["success"]
+      end
+
+      should "failed using the same client" do
+        @server.send(:login, @logged_client, @new_username)
+        response_json = @logged_client.response
+        result = JSON.parse(response_json)
+        assert_false result["success"]
+      end
+
+      should "failed using the existing name" do
+        @server.send(:login, @new_client, @logged_username)
+        response_json = @new_client.response
+        result = JSON.parse(response_json)
+        assert_false result["success"]
+      end
     end
+  end
+end
+
+class Client
+  attr_reader :response
+
+  def initialize(name)
+    @name = name
+  end
+
+  def puts(message)
+    @response = message
   end
 end
