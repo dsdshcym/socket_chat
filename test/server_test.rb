8f014394eb55a828fc06e7850ca080d9cdeaa939
Yiming Chen <dsdshcym@gmail.com>
2015-11-26 09:38:36 +0800

    Add test for create an existing channel

--- test/server_test.rb
+++ test/server_test.rb
@@ -133,6 +133,24 @@ class TestServer < Test::Unit::TestCase
         @server.instance_eval { channels = @channels }
         assert_equal [@new_channel], channels
       end
+
+      context "try to create an existing channel" do
+        setup do
+          @old_channel = "OC"
+          old_channel = @old_channel
+          @server.instance_eval { @channels = [old_channel] }
+        end
+
+        should "failed" do
+          @server.send(:create, @logged_client, @old_channel)
+          response_json = @logged_client.response
+          result = JSON.parse(response_json)
+          assert_false result["success"]
+          channels = nil
+          @server.instance_eval { channels = @channels }
+          assert_equal [@old_channel], channels
+        end
+      end
     end
   end
 end
